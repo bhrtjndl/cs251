fda58d094a20131711c426c8df58e772bf3f4ef1
USER_1 <bhartj81@gmail.com>
2018-03-04 19:30:27 +0530

    Revert "Step 26"
    This reverts commit 2c4c738ab47e489ecd03788509205573f3bfbe19.

--- hello.c
+++ hello.c
@@ -6,7 +6,6 @@ void main(){
 	printf("Helloworld!\n");
 	printf("This must be a monolithic design\n");
 	microkernel_sendmsg("is more portable");
-	putting_this_error();
 }
 
 void microkernel_sendmsg(char *a){
